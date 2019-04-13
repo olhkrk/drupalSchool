ef4ba8f4a32b46dff08898d0cbafa432c316583f
olhkrk <olhkrk@gmail,com>
2019-04-13 09:07:18 +0300

    first commit

---
+++ task.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>forgit</title>
+</head>
+<body>
+<p></p>
+
+
+</body>
+</html>
+
+<?php
+
+
+$value = intdiv(10,3);
+var_dump($value);
+echo (" ");
+echo ($value);
+
+
+?>
+
+
